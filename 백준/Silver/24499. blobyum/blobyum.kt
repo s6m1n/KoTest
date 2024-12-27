63fbfc732852a92ba80062065a122d6325ca4be5
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2024-12-27 16:45:49 +0900

    [Silver IV] Title: blobyum, Time: 284 ms, Memory: 31984 KB -BaekjoonHub

--- 백준/Silver/24499. blobyum/blobyum.kt
+++ 백준/Silver/24499. blobyum/blobyum.kt
@@ -5,17 +5,16 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val input = br.readLine().split(" ").map { it.toInt() } // 총 N개의 파이, K개를 먹는다
     val tastes = br.readLine().split(" ").map { it.toInt() } // N개 파이의 맛있음 값
-    val sumOfTastes = IntArray(input[0]) // N개 있어야 함
-    sumOfTastes[0] = tastes.take(input[1]).sumOf { it }
-    var max = sumOfTastes[0]
+    var target = tastes.take(input[1]).sumOf { it }
+    var max = target
     var start = 1
     var end = input[1] // K
     while (start < tastes.size) {
         if(end == tastes.size) {
             end = 0
         }
-        sumOfTastes[start] = sumOfTastes[start - 1] - tastes[start - 1] + tastes[end]
-        if (max < sumOfTastes[start]) max = sumOfTastes[start]
+        target = target - tastes[start - 1] + tastes[end]
+        if (max < target) max = target
         end++
         start++
     }
