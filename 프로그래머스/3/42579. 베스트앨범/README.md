3e70be816e12123cd1746982c2384e4213b412de
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2025-02-04 10:48:20 +0900

    [level 3] Title: 베스트앨범, Time: 44.12 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 66.8 MB, 시간: 27.71 ms
+메모리: 67.2 MB, 시간: 44.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 03일 17:33:13
+2025년 02월 04일 10:48:15
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.kt
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.kt
@@ -1,20 +1,17 @@
 class Solution {
     fun solution(genres: Array<String>, plays: IntArray): IntArray {
-        var answer = intArrayOf()
-        val sort = mutableMapOf<String, MutableList<Pair<Int, Int>>>()
-        for ((idx, genre) in genres.withIndex()) {
-            sort[genre]?.add(plays[idx] to idx) ?: run {
-                sort[genre] = mutableListOf(plays[idx] to idx)
+        val answer = genres.indices
+            .groupBy { genres[it] } // idx 기준으로 groupBy -> Map<String, list>
+            .toList()
+            .sortedByDescending { it.second.sumOf { idx -> plays[idx] } } // 총 재생 수가 많은 장르 순으로 정렬
+            .map {
+                it.second
+                    .sortedByDescending { idx -> plays[idx] } // 재생 수가 높은 노래 순으로 정렬
+                    .take(2) // 상위 2개만
             }
-        }
-        val sortedList = sort.entries.sortedBy { -it.value.sumOf { it.first } }
-        for (genreSongs in sortedList) {
-            val sortedGenre = genreSongs.value.sortedBy { -it.first }
-            for(songIdx in sortedGenre.indices){
-                if(1 < songIdx) break
-                answer += intArrayOf(sortedGenre[songIdx].second)
-            }
-        }
+            .flatten()
+            .toIntArray()
         return answer
     }
+
 }
