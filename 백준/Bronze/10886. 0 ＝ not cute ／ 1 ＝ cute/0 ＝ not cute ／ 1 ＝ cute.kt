768e1d8194bf2b0829f0a635911133427174c52d
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2025-03-10 16:19:14 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 76 ms, Memory: 12108 KB -BaekjoonHub

--- 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.kt
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.kt
@@ -5,9 +5,7 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val N = readLine().toInt()
     var cnt = 0
     repeat(N) {
-        if (readLine().toInt() == 0) cnt--
-        else cnt++
+        cnt += if (readLine().toInt() == 0) -1 else +1
     }
-    if (cnt < 0) println("Junhee is not cute!")
-    else println("Junhee is cute!")
+    println(if (cnt < 0) "Junhee is not cute!" else "Junhee is cute!")
 }
