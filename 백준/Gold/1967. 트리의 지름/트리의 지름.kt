299654a944805554634ecb2d3eb19e35aac7836c
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2025-03-18 16:08:26 +0900

    [Gold IV] Title: 트리의 지름, Time: 288 ms, Memory: 31268 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.kt
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.kt
@@ -2,35 +2,29 @@ fun main() {
     val n = readln().toInt() // 노드의 개수 (1 ≤ n ≤ 10,000)
     val list = List(n + 1) { mutableListOf<IntArray>() }
     var visited = BooleanArray(n + 1) { false }
-    val checked = BooleanArray(n + 1) { false }
-    val candidates = (1..n).toMutableList()
     repeat(n - 1) {
         val (start, end, weight) = readln().split(" ").map { it.toInt() }
         list[start].add(intArrayOf(end, weight))
         list[end].add(intArrayOf(start, weight))
-        candidates.remove(start)
     }
-    fun dfs(origin: Int, start: Int): Int {
-        visited[start] = true // 방문 처리
-        if (list[start].size == 1 && start != origin) { // 말단이면
-            return 0
-        }
-        var max = 0
-        for (next in list[start]) { // 연결된 노드 중
-            if (!visited[next[0]] && !checked[next[0]]) { // 방문하지 않은 곳만 체크
-                val tmp = dfs(origin, next[0]) + next[1]
-                if (max < tmp) max = tmp
+    var max = 0
+    var max_idx = 0
+    fun dfs(origin: Int, start: Int, sum: Int) {
+        visited[start] = true
+        if (list[start].size == 1 && origin != start) {
+            if (max < sum) {
+                max = sum
+                max_idx = start
             }
         }
-        return max // origin에서 갈 수 있는 최대 거리
+        for (next in list[start]) {
+            if (!visited[next[0]]) {
+                dfs(origin, next[0], sum + next[1])
+            }
+        }
     }
-
-    var max = 0
-    for (start in candidates) {
-        checked[start] = true
-        val tmp = dfs(start, start)
-        if (max < tmp) max = tmp
-        visited = BooleanArray(n + 1) { false }
-    }
+    dfs(1, 1, 0)
+    visited.fill(false)
+    dfs(max_idx, max_idx, 0)
     println(max)
 }
