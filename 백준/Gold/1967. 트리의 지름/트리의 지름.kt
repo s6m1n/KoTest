971dd0515f0874ecfc5a9e6dbe41332289b78604
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2025-03-18 16:17:10 +0900

    [Gold IV] Title: 트리의 지름, Time: 280 ms, Memory: 33396 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.kt
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.kt
@@ -1,7 +1,7 @@
 fun main() {
     val n = readln().toInt() // 노드의 개수 (1 ≤ n ≤ 10,000)
     val list = List(n + 1) { mutableListOf<IntArray>() }
-    var visited = BooleanArray(n + 1) { false }
+    val visited = BooleanArray(n + 1) { false }
     repeat(n - 1) {
         val (start, end, weight) = readln().split(" ").map { it.toInt() }
         list[start].add(intArrayOf(end, weight))
@@ -9,22 +9,20 @@ fun main() {
     }
     var max = 0
     var max_idx = 0
-    fun dfs(origin: Int, start: Int, sum: Int) {
+    fun dfs(start: Int, sum: Int) {
         visited[start] = true
-        if (list[start].size == 1 && origin != start) {
-            if (max < sum) {
-                max = sum
-                max_idx = start
-            }
+        if (max < sum) {
+            max = sum
+            max_idx = start
         }
-        for (next in list[start]) {
-            if (!visited[next[0]]) {
-                dfs(origin, next[0], sum + next[1])
+        for ((next, weight) in list[start]) {
+            if (!visited[next]) {
+                dfs(next, sum + weight)
             }
         }
     }
-    dfs(1, 1, 0)
+    dfs(1, 0)
     visited.fill(false)
-    dfs(max_idx, max_idx, 0)
+    dfs(max_idx, 0)
     println(max)
 }
