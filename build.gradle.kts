74a90453583d9004554f39ba2bb6b1d4ec134e73
somin <salth0601@gmail.com>
2025-04-24 12:54:22 +0900

    코루틴 및 테스트 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.8.1")
+    testImplementation("org.jetbrains.kotlinx", "kotlinx-coroutines-test", "1.8.1")
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.10.2")
+    testImplementation("org.assertj", "assertj-core", "3.25.3")
+    testImplementation("io.kotest", "kotest-runner-junit5", "5.8.0")
+    testImplementation("io.mockk:mockk:1.13.8")
 }
 
 tasks.test {
