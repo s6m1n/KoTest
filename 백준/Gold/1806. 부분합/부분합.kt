12c124478e5e750fdcbb946f06293df7af783a03
Somin Lee <salth0601@gmail.com>
2025-06-09 01:33:17 +0900

    [Gold IV] Title: 부분합, Time: 296 ms, Memory: 34308 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.kt
@@ -0,0 +1,16 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val (N, S) = readLine().split(" ").map { it.toInt() }
+    val arr = readLine().split(" ").map { it.toInt() }
+    var left = 0
+    var right = 0
+    var sum = 0
+    var min = Int.MAX_VALUE
+    while (right < N) {
+        sum += arr[right++]
+        while (S <= sum && left < right) { // left 이동시킬 조건
+            sum -= arr[left++]
+            min = minOf(min, right - left + 1)
+        }
+    }
+    println(if (min == Int.MAX_VALUE) 0 else min)
+}
